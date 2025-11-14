d8fb83513faa6b5436ce7b3d0d748b321a4a6bc9
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-11-13 19:49:27 -0800

    fix: images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       new URL("https://avatars.githubusercontent.com/**?v=4"),
       new URL("https://http.cat/**")
