22c696d51d39724b1ac660dbb9f24a4a4e1e4e3e
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-11-26 21:50:15 -0800

    feat: add rss link to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import PostList from "./components/post-list";
 import { getSortedPosts } from "./util/posts";
 import { generateRss } from "./util/rss";
+import { Rss } from "lucide-react";
 
 export default async function Home() {
   const posts = getSortedPosts();
@@ -10,7 +12,12 @@ export default async function Home() {
 
   return (
     <div className="py-8">
-      <h1>Recent Posts</h1>
+      <div className="flex flex-row justify-between items-center">
+        <h1>Recent Posts</h1>
+        <Link className="mx-1" href="/rss.xml" target="_blank" rel="noopener noreferrer">
+          <Rss />
+        </Link>
+      </div>
 
       <PostList posts={posts} />
     </div>
