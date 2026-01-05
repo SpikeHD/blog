b1cc059caf59b3130f21c13dd9a0f48529f8cdc1
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2026-01-05 12:59:00 -0800

    fix: tag meta

--- app/by-tag/[tag]/page.tsx
+++ app/by-tag/[tag]/page.tsx
@@ -1,10 +1,21 @@
 import { BackToHome } from "@/app/components/back-to-home";
 import PostList from "@/app/components/post-list";
+import { SITE_NAME } from "@/app/constants";
 import { getAllUniqueTags, getPostsWithTag } from "@/app/util/posts";
+import { Metadata } from "next";
 
 export function generateStaticParams() {
   const tags = getAllUniqueTags();
   return tags.map((tag) => ({ tag }));
+}
+
+export async function generateMetadata({ params }: { params: Promise<{ tag: string }> }): Promise<Metadata> {
+  const { tag } = await params;
+
+  return {
+    title: `Tagged with "${tag}" - ${SITE_NAME}`,
+    description: `List of all blog posts tagged with ${tag}.`
+  };
 }
 
 export default async function ByTagPage({ params }: {
